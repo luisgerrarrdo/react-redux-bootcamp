11b2d0ff1da851869d820ed72ed5d2403b046f93
Luis Gerardo <LuisGerrarrdo@hotmail.com>
2022-07-12 23:37:47 -0700

    add login and cart ui

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,148 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Summary from "../components/Summary";
+import { BASE_API_URL } from "../constants/global";
+
+const Container = styled.main`
+  width: 70%;
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  justify-content: center;
+  margin-top: 32px;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: row;
+  -webkit-box-pack: center;
+  justify-content: center;
+`;
+
+const DetailsImage = styled.div`
+  height: 150px;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+`;
+
+const Td = styled.td`
+  padding: 16px;
+`;
+
+const Hr = styled.hr`
+  width: 95%;
+  height: 1px;
+  background-color: rgb(211, 211, 212);
+  border: none;
+`;
+
+const Image = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+`;
+
+const DetailsDescription = styled.div`
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  margin-left: 32px;
+  flex-direction: column;
+  max-width: 300px;
+`;
+
+const Aside = styled.aside`
+  margin-left: 32px;
+  margin-top: 32px;
+  padding-top: 0px;
+  flex-direction: column;
+  -webkit-box-pack: start;
+  justify-content: flex-start;
+`;
+
+const Quatity = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  width: 50%;
+  margin: auto;
+`;
 
 export const Cart = () => {
+  const [cartItems, setcartItems] = useState([]);
+
+  async function getProducts() {
+    try {
+      const response = await axios.get(BASE_API_URL);
+      const { products: productsResponse } = response.data.data;
+
+      setcartItems(
+        productsResponse.items
+          .slice(-2)
+          .map((item) => ({ ...item, quantity: 1 }))
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
-    <div>Cart</div>
-  )
-}
+    <Container>
+      <Content>
+        <h3>Shopping Cart</h3>
+        <Hr />
+        <table>
+          <thead>
+            <tr>
+              <th>Product details</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map(({ id, images, name, quantity, price }) => (
+              <tr key={id}>
+                <Td>
+                  <Details>
+                    <DetailsImage>
+                      <Image src={images[0]} alt={name} />
+                    </DetailsImage>
+                    <DetailsDescription>
+                      <p>{name}</p>
+                      <p>{`Product code: ${id}`}</p>
+                    </DetailsDescription>
+                  </Details>
+                </Td>
+                <Td>
+                  <Quatity>
+                    <input type="number" value={quantity} />
+                    <button type="button">Remove</button>
+                  </Quatity>
+                </Td>
+                <Td>{`$ ${price}`}</Td>
+                <Td>{`$ ${Number(price) * Number(quantity)}`}</Td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Content>
+      <Aside>
+        <Summary />
+      </Aside>
+    </Container>
+  );
+};
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,131 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components";
+import loginApi from "../utils/loginApi";
+
+const Container = styled.section`
+  display: flex;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  margin-top: 128px;
+  height: 450px;
+  width: 600px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(245, 245, 245);
+`;
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 75%;
+`;
+
+const Title = styled.h1`
+  font-size: 2em;
+`;
+
+const Hr = styled.hr`
+  margin: 16px 0;
+  width: 80%;
+  height: 1px;
+  background-color: rgb(211, 211, 212);
+  border: none;
+`;
+
+const Input = styled.input`
+  font-size: 1rem;
+  margin-bottom: 16px;
+  margin-top: 8px;
+  border: 1px solid rgba(204, 204, 204, 0.8);
+  line-height: 1.5;
+  padding: 8px;
+`;
+
+const Button = styled.button`
+  margin-top: 16px;
+  width: 100%;
+  background-color: rgb(32, 52, 73);
+  color: white;
+  border: 0px;
+  height: 50px;
+  cursor: pointer;
+  transition: all 0.3s ease 0s;
+`;
+
+const SignInButton = styled.button`
+  padding: 8px;
+  display: flex;
+  font-size: 16px;
+  margin-bottom: 32px;
+`;
+
+const ImageContainer = styled.div`
+  width: 16px;
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+`;
 
 export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const handleLogin = async () => {
+    try {
+      console.log("trying to login...");
+      const response = await loginApi(username, password);
+      console.log(`login response: ${response}`);
+      history.push("/products");
+    } catch (e) {
+      alert(e);
+      console.error(e);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <Container>
+      <Content>
+        <Title>Welcome to the WizeStore!</Title>
+        <Form>
+          <label htmlFor="usr">
+            <b>Username</b>
+          </label>
+          <Input
+            type="text"
+            placeholder="Enter Username"
+            name="usr"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="psw">
+            <b>Password</b>
+          </label>
+          <Input
+            type="password"
+            placeholder="Enter Password"
+            name="psw"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button onClick={handleLogin}>Login</Button>
+        </Form>
+        <Hr />
+        <SignInButton>
+          <ImageContainer>
+            <Image src="google_g_logo.png" alt="Google logo" />
+          </ImageContainer>
+          Sign in with Google
+        </SignInButton>
+      </Content>
+    </Container>
+  );
+};
